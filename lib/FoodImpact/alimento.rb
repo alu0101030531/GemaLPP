926b4daca6e9ad2d33aa618fda018e0deb3e4a2f
Christian <alu0101030531@ull.edu.es>
2019-11-11 22:33:46 +0000

    Tiene unos lipidos asociados al alimento

--- lib/FoodImpact/alimento.rb
+++ lib/FoodImpact/alimento.rb
@@ -1,7 +1,7 @@
  
 class Alimento 
   attr_reader :nombre, :proteinas, :carbohidratos
-  def initialize(nombre, proteinas, carbohidratos)
-    @nombre, @proteinas, @carbohidratos = nombre, proteinas, carbohidratos
+  def initialize(nombre, proteinas, carbohidratos, lipidos)
+    @nombre, @proteinas, @carbohidratos, @lipidos = nombre, proteinas, carbohidratos, lipidos
   end
 end
