5e412b00bd7ab6b629983d050daf8f327c8db210
Christian <alu0101030531@ull.edu.es>
2019-11-16 23:26:23 +0000

    La clase dieta no tiene un constructor

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -31,6 +31,23 @@ RSpec.describe List do
 end
 
 RSpec.describe Dieta do
+  before :each do 
+    @carne_de_vaca = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
+    @nuez = Alimento.new("Nuez", 20.0, 21.0, 54.0 ,0.3, 7.9)
+    @pollo = Alimento.new("Pollo", 20.6, 0.0, 5.6, 5.7, 7.1)
+    @huevos = Alimento.new("Huevos", 13.0, 1.1, 11.0, 4.2, 5.7)
+    @cerveza = Alimento.new("Cerveza", 0.5, 3.6, 0.0, 0.24, 0.22)
+    @food_list = List.new(nil, nil)
+    @food_list.insert(@carne_de_vaca)
+    @food_list.insert(@nuez)
+    @food_list.insert(@pollo)
+    @food_list.insert(@huevos)
+    @food_list.insert(@cerveza)
+  end
+
+  it "La clase dieta tiene un constructor" do
+    dieta = Dieta.new(@food_list)  
+  end
 end
 
 
