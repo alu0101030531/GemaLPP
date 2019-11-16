e684790148f6e98f9d664754e95985c06a6c67bc
Christian <alu0101030531@ull.edu.es>
2019-11-16 21:42:56 +0000

    Falla al extraer el primer elemento

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,7 +1,12 @@
 RSpec.describe List do
   before :each do
     @carne_de_vaca = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
+    @nuez = Alimento.new("Nuez", 20.0, 21.0, 54.0 ,0.3, 7.9)
+    @pollo = Alimento.new("Pollo", 20.6, 0.0, 5.6, 5.7, 7.1)
+    @huevos = Alimento.new("Huevos", 13.0, 1.1, 11.0, 4.2, 5.7)
+    @cerveza = Alimento.new("Cerveza", 0.5, 3.6, 0.0, 0.24, 0.22)
     @food_list = List.new(nil, nil)
+    @food_list.insert(@carne_de_vaca)
   end
 
   it "Debe existir un constructor" do
@@ -9,11 +14,14 @@ RSpec.describe List do
   end
 
   it "Debe poder insertar" do
-    @food_list.insert(@carne_de_vaca)
+    @food_list.insert(@nuez)
   end
 
   it "Extrae el último elemento" do
-    @food_list.insert(@carne_de_vaca)
     expect(@food_list.last).to eq(@carne_de_vaca)
+  end
+  
+  it "Extrae el primer elemento" do
+    expect(@food_list.first).to eq(@carne_de_vaca)
   end
 end
