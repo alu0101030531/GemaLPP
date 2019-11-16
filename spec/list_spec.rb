e01d2f77c2d62c2b4e4907719b733f60dfee6581
Christian <alu0101030531@ull.edu.es>
2019-11-16 21:27:10 +0000

    Falla al preguntar por el método insert

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,5 +1,14 @@
 RSpec.describe List do
+  before :each do
+    @carne_de_vaca = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
+    @food_list = List.new(nil, nil)
+  end
+
   it "Debe existir un constructor" do
-    @test_list = List.new(nil,nil)
+    @test_list = List.new(nil, nil)
+  end
+
+  it "Debe poder insertar" do
+    @food_list.insert(@producto)
   end
 end
