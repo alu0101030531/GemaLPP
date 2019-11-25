c3ecf2ba67f009b5554bfbce6d53d13aeda133ac
Christian <alu0101030531@ull.edu.es>
2019-11-25 18:58:48 +0000

    Existe una clase plato

--- lib/FoodImpact.rb
+++ lib/FoodImpact.rb
@@ -2,6 +2,7 @@ require "FoodImpact/version"
 require "FoodImpact/alimento"
 require "FoodImpact/list"
 require "FoodImpact/dieta"
+require "FoodImpact/plato"
 
 module FoodImpact
   class Error < StandardError; end
