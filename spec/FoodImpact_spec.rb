a7d55ce384f0ae5d1d50b193c62baa7866e474eb
Christian <alu0101030531@ull.edu.es>
2019-11-25 16:18:09 +0000

    Compara usando <=

--- spec/FoodImpact_spec.rb
+++ spec/FoodImpact_spec.rb
@@ -55,5 +55,8 @@ RSpec.describe Alimento do
     it "operador ==" do
       expect(@carne_de_vaca == @carne_de_vaca).to eq(true)
     end
+    it "operador <=" do
+      expect(@Pollo <= @carne_de_vaca).to eq(true)
+    end
   end
 end
