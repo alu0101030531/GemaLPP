4f67b45ab496257685b315ed01bc553e1f36ad9b
Christian <alu0101030531@ull.edu.es>
2019-11-25 19:07:03 +0000

    Falla al pasarle un conjunto de alimentos

--- spec/plato_spec.rb
+++ spec/plato_spec.rb
@@ -1,6 +1,21 @@
 RSpec.describe Plato do
   before :each do
-    @plato = Plato.new("Escaldon")
+    @plato_list = List.new(nil, nil)
+    @carne_de_vaca = Alimento.new("Carne de vaca", 21.1, 0.0, 3.1, 50.0, 164.0)
+    @nuez = Alimento.new("Nuez", 20.0, 21.0, 54.0 ,0.3, 7.9)
+    @pollo = Alimento.new("Pollo", 20.6, 0.0, 5.6, 5.7, 7.1)
+    @huevos = Alimento.new("Huevos", 13.0, 1.1, 11.0, 4.2, 5.7)
+    @cerveza = Alimento.new("Cerveza", 0.5, 3.6, 0.0, 0.24, 0.22)
+    @carne_de_cordero = Alimento.new("Carne de cordero", 18.0, 0.0, 17.0, 20.0, 185.0)
+    @camarones = Alimento.new("Camarones", 17.6, 1.5, 0.6, 18.0, 2.0)
+    @chocolate = Alimento.new("Chocolate", 5.3, 47.0, 30.0, 2.3, 3.4)
+    @salmon = Alimento.new("Salmon", 19.9, 0.0, 13.6, 6.0, 3.7)
+    @tofu = Alimento.new("Tofu", 8.0, 1.9, 4.8, 2.0, 2.2)
+    @lentejas = Alimento.new("Lentejas", 23.5, 52.0, 1.4, 0.4, 3.4)
+    @plato_list.insert(@lentejas)
+    @plato_list.insert(@tofu)
+    @plato_list.insert(@salmon)
+    @plato = Plato.new("Escaldon", @plato_list)
   end
 
   it "Nombre del plato" do
